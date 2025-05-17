7fe593f574eb00fcfabed61c66374bf9de69b11f
riyo <g.riyo369@gmail.com>
2025-05-17 14:52:58 +0900

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,30 @@
+
+
+let num15 =(Math.random() * 5);
+
+if (num15 % 3 === 0 && num % 5 === 0 ){
+  console.log('3と5の倍数です');
+}
+  
+else if(num15 % 3 === 0){
+  console.log('3の倍数です');
+}
+
+
+else if (num15 % 5 == 0){
+  console.log('5の倍数です');
+}
+
+else{
+  console.log(num15);
+}
+
+
+
+
+
+
+
+
+
+
